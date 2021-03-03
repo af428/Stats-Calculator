a09d0b183e19d40dffd8c74a403d878d27a92713
af428 <56081056+af428@users.noreply.github.com>
2021-03-03 14:00:38 -0500

    feature: import static methods and call them

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,15 +1,37 @@
-def addition(a,b):
-    c = a + b
-    return a + b
+from src.operations.addition import Addition
+from src.operations.divsion import Division
+from src.operations.multiplication import Multiplication
+from src.operations.square import Square
+from src.operations.square_root import SqaureRoot
+from src.operations.subtraction import Subtraction
+
 
 class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
         pass
 
-    def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+    def add(self, x, y):
+        self.result = Addition.addition(x, y)
+        return self.result
+
+    def subtract(self, x, y):
+        self.result = Subtraction.subtraction(x, y)
+        return self.result
+
+    def multiply(self, x, y):
+        self.result = Multiplication.multiply(x, y)
+        return self.result
+
+    def divide(self, x, y):
+        self.result = round(Division.division(x, y), 9)
+        return self.result
+
+    def square(self, x):
+        self.result = Square.square(x)
+        return self.result
+
+    def square_root(self, x):
+        self.result = SquareRoot.square_root(x)
+        return self.result
