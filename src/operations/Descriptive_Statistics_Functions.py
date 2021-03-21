326926a1041222635484b6b47f1fc9a3a583cfb2
LukaDragoljic <ld293@njit.edu>
2021-03-21 19:05:09 +0100

    Task: Imported basic functions to descriptive statistics functions

---
+++ src/operations/Descriptive_Statistics_Functions.py
@@ -0,0 +1,6 @@
+from src.operations import addition
+from src.operations import divsion
+from src.operations import multiplication
+from src.operations import square
+from src.operations import square_root
+from src.operations import subtraction
