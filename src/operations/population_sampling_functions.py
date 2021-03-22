d54df8c2341750b91ff65f0beebdc495b676732b
af428 <56081056+af428@users.noreply.github.com>
2021-03-22 19:32:19 -0400

    feature: added three of the functions

--- src/operations/population_sampling_functions.py
+++ src/operations/population_sampling_functions.py
@@ -10,3 +10,24 @@ from scipy.stats import sem, t
 class PopulationSamplingFunctions:
     @staticmethod
     def cochrans (input_list):
+        error = sem(input_list)
+        gstd = stats.gstd(input_list)
+        z = stats.szcore(input_list)
+        x = (((z ** 2) * .25) / (error ** 2))
+        return int(x)
+
+    @staticmethod
+    def confidence_int_sample(confidence, sample_list):
+        x = len(sample_list)
+        average = numpy.mean(sample_list)
+        std_error_mean = sem(sample_list)
+        interval = std_error_mean * t.ppf((1 +confidence)/2, x - 1)
+        high = average + interval
+        low = average - interval
+        return high, low
+
+    @staticmethod
+    def margin_of_error(input_list):
+        return sem(input_list)
+
+
