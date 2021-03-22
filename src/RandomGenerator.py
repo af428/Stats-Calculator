a49e9dac6cf754ca71d60737592344807f8d2d68
Charles Wicklund <chwicklund@gmail.com>
2021-03-22 08:39:28 -0400

    Feature: added randomNumSeed class and randomNumsSeed class

--- src/RandomGenerator.py
+++ src/RandomGenerator.py
@@ -1,10 +1,17 @@
 import random
 from src.randomOps import randomNumber, randomNumberWithSeed, randomNumbersWithSeed, randItemFromList, randItemFromListSeed, randItemsFromList, randItemsFromListSeed
 
+
 class RandomGenerator:
 
     def __init__(self):
         pass
 
-    def randomNum(self, start, end):
-        return randomNumber.randomNum(start, end).getResult()
+    def randomNum(self, first, last):
+        return randomNumber.randomNum(first, last).getResult()
+
+    def randomNumSeed(self, first, last, seed):
+        return randomNumberWithSeed.randomNumSeed(first, last, seed).getResult()
+
+    def randomNumsSeed(self, first, last, n, seed):
+        return randomNumberWithSeed.randomNumSeed(first, last, seed).getResult()
