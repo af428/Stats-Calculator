cbc75188294f0838b70f18a6aaf054c2e14cb4de
af428 <56081056+af428@users.noreply.github.com>
2021-03-22 19:22:28 -0400

    feature: needed to import modules for standard error of mean, distribution

--- src/operations/population_sampling_functions.py
+++ src/operations/population_sampling_functions.py
@@ -1,6 +1,12 @@
+import statistics
+import random
 from src.operations.Descriptive_Statistics_Functions import StatisticsFunctions
 from src.randomOps.randomNumberWithSeed import randomNumSeed
 from src.RandomGenerator import RandomGenerator
 from src.randomOps import randomNumber, randomNumberWithSeed, randomNumbersWithSeed, randItemFromList, randItemsFromList, randItemFromListSeed, randItemsFromListS
-import statistics
+from scipy import stats
+from scipy.stats import sem, t
 
+class PopulationSamplingFunctions:
+    @staticmethod
+    def cochrans (input_list):
