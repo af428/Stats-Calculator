4f89ef978f1e735d7f328eb6be3db08f0288ab1a
af428 <56081056+af428@users.noreply.github.com>
2021-03-22 20:11:36 -0400

    fix: confidence interval

--- src/operations/population_sampling_functions.py
+++ src/operations/population_sampling_functions.py
@@ -8,8 +8,35 @@ from scipy import stats
 from scipy.stats import sem, t
 
 class PopulationSamplingFunctions:
+
     @staticmethod
-    def cochrans (input_list):
+    # Simple Random Sampling
+    def sample_list(input_list, new_list_length):
+        if not input_list:
+            return "Error: input list was empty"
+        if new_list_length > len(input_list):
+            return "Error: sample list length is bigger than original list"
+        return random.sample(input_list, new_list_length)
+
+
+    @staticmethod
+    # Confidence Interval For a Sample
+    def confidence_int_sample(confidence, sample_list):
+        sample_size = len(sample_list)
+        sample_mean = numpy.mean(sample_list)
+        sample_std = sem(sample_list)
+        degrees_freedom = sample_size - 1
+        confidence_interval = t.interval(confidence, degrees_freedom, sample_std)
+        return confidence_interval
+
+    @staticmethod
+    # Margin of Error
+    def margin_of_error(input_list):
+        return sem(input_list)
+
+    @staticmethod
+    # Cochran’s Sample Size Formula
+    def cochrans(input_list):
         error = sem(input_list)
         gstd = stats.gstd(input_list)
         z = stats.szcore(input_list)
@@ -17,17 +44,12 @@ class PopulationSamplingFunctions:
         return int(x)
 
     @staticmethod
-    def confidence_int_sample(confidence, sample_list):
-        x = len(sample_list)
-        average = numpy.mean(sample_list)
-        std_error_mean = sem(sample_list)
-        interval = std_error_mean * t.ppf((1 +confidence)/2, x - 1)
-        high = average + interval
-        low = average - interval
-        return high, low
-
-    @staticmethod
-    def margin_of_error(input_list):
-        return sem(input_list)
-
-
+    # Sample Size Given a Confidence Interval and Width
+    def sample_size(input_list):
+        # confidence = 0.05
+        error = sem(input_list)
+        std = stats.gstd(input_list)
+        # z = stats.zscore(input_list)
+        z = 1.96
+        n = ((z * std) / error) ** 2
+        return int(n)
